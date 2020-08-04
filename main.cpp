a37407422c29b6836c039c39a58108df9ac0e768
cj <chrisjwas@gmail.com>
2020-08-03 17:22:41 -0700

    Accepting clients

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdint-uintn.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/epoll.h>
@@ -13,6 +14,8 @@
 // 	send back response 
 /* Initializes socket to accept connections */
 
+const int MAX_EVENTS = 10;
+int ep_ret;
 int conn_sockfd;
 int cli_sockfd;
 int port = 3000;
@@ -26,7 +29,9 @@ void error(const char *msg) {
 
 void init_sockets(){
     conn_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
+    int flags = fcntl(conn_sockfd, F_GETFL, 0);
+    fcntl(conn_sockfd, flags | SOCK_NONBLOCK);
+
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
@@ -40,17 +45,33 @@ void init_sockets(){
 
 };
 
+void register_event(int epoll_fd) {
+    struct epoll_event event;
+    uint32_t ep_events;
+    event.data.fd = cli_sockfd;
+    event.events = ep_events;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, cli_sockfd, &event);
+};
+
 int main() {
     init_sockets();
+    int epoll_fd = epoll_create1(0);
+    struct epoll_event events;
     while (1) {
 	cli_sockfd = accept(conn_sockfd, (struct sockaddr *) &cli_addr, &size);
 	printf("Connection accepted\n");
-	int flags = fnctl(cli_sockfd, F_GETFL, 0);
-	fcntl(cli_sockfd, flags | SOCK_NONBLOCK);
+
+	register_event(epoll_fd);
+        ep_ret = epoll_wait(epoll_fd, &events, MAX_EVENTS, 0);
+	if (ep_ret < 0) {
+	    error("epoll_wait()");
+	};
+	for (int i = 0; i < ep_ret; i++) {
+	    
+	}
+
 
     };
-    // accept multiple clients
-    // send back response
     
     return 0;
 };
